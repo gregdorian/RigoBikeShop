be89c2cb08c87049eb32fe209fb9e10bdfbc9fc9
german valencia <gerger2024@hotmail.com>
2022-11-25 10:47:39 -0500

    Images

--- rigorigoBikeshop.UIWebApi/Program.cs
+++ rigorigoBikeshop.UIWebApi/Program.cs
@@ -1,7 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//private static readonly string cadenaConexion = ConfigurationManager.ConnectionStrings["sqlConexionApp"].ConnectionString;
+private static readonly string cadenaConexion = "Data Source=ACER-5151\\SQLEXPRESS01;Initial Catalog=cyclingbikeshop;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+private static SqlConnection conexion = new SqlConnection(cadenaConexion);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
