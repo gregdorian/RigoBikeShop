30d3d85548cf8880bb6a0f95e6b428d728a6c2c0
german <ger.ma@hotmail.com>
2022-11-28 11:37:08 -0500

    correcciones a javascript

--- rigorigoBikeshop.UIWebApi/Program.cs
+++ rigorigoBikeshop.UIWebApi/Program.cs
@@ -6,15 +6,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
 //services cors
 builder.Services.AddCors(p => p.AddPolicy("MVC", builder =>
 {
 
     builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost").AllowAnyMethod().AllowAnyHeader();
 }));
+
+
 var app = builder.Build();
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
